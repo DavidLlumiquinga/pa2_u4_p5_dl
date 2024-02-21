da9b381e27854068f88139f0143ff75bb57ca05c
unknown <mdllumiquingat@uce.edu.ec>
2024-02-20 20:36:30 -0500

    Taller37

---
+++ src/main/java/pa2_u3_p5_dl/Pa2U4P5DlApplication.java
@@ -0,0 +1,13 @@
+package pa2_u3_p5_dl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pa2U4P5DlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pa2U4P5DlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pa2_u3_p5_dl/Pa2U4P5DlApplicationTests.java
@@ -0,0 +1,13 @@
+package pa2_u3_p5_dl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pa2U4P5DlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
